936ba7abac5c6d2eda8fed925aed33e39c260fd6
PIYUSH SRIVASTAVA <166242534+Piyush28122003@users.noreply.github.com>
2025-02-27 13:35:39 +0530

    Add files via upload

---
+++ AI_Mock_Interview/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Subscribe to ShashOnTop!</h2>
+      <Button>Subscribe</Button>
+    </div>
+  );
+}
